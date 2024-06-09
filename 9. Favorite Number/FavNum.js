bda12003f2849b552eca78c8cf0c5cb39c88e9af
Sahar Mughal <saharmughal2022@gmail.com>
2024-06-09 17:53:37 +0500

    Add files via upload

---
+++ 9. Favorite Number/FavNum.js
@@ -0,0 +1,6 @@
+"use strict";
+const favorate_number = "2";
+const message = `My favorate number is ${favorate_number}`;
+console.log(message);
+console.log(`${message}, ${favorate_number}`);
+console.log(message, favorate_number);
---
+++ 9. Favorite Number/FavNum.ts
@@ -0,0 +1,9 @@
+const favorate_number: string = "2"
+
+const message:string = `My favorate number is ${favorate_number}`;
+
+console.log(message);
+
+console.log(`${message}, ${favorate_number}`);
+
+console.log(message , favorate_number);
