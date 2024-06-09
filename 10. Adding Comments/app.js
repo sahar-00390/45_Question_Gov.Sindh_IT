12ee853f4298729896e8538291155a79866be0a7
Sahar Mughal <saharmughal2022@gmail.com>
2024-06-09 23:25:03 +0500

    Add files via upload

---
+++ 10. Adding Comments/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Name: Sahar Mughal
+// Date: 9 June 2024
+Object.defineProperty(exports, "__esModule", { value: true });
+// making a variable of peotName which containt famous Poet name
+let poetName = "Joun Elia";
+// making a variabl of massage with contain two lines poerty 
+let poetry = "\n Mery Kamry ko sajany ki tamana hai tumhy, \n Mery kamry me kitabon k siwa kuch bhi nahi";
+// print the poet name and his poetry by using log
+console.log(`${poetName}, said "${poetry}"`);
---
+++ 10. Adding Comments/app.ts
@@ -0,0 +1,14 @@
+// Name: Sahar Mughal
+// Date: 9 June 2024
+
+// making a variable of peotName which containt famous Poet name
+
+let poetName:string = "Joun Elia"
+
+
+// making a variabl of massage with contain two lines poerty 
+
+let poetry: string = "\n Mery Kamry ko sajany ki tamana hai tumhy, \n Mery kamry me kitabon k siwa kuch bhi nahi";
+
+// print the poet name and his poetry by using log
+console.log(`${poetName}, said "${poetry}"`);
