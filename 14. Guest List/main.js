4318fd0b37eaca21f8c55818be3900441f0089e9
Sahar Mughal <saharmughal2022@gmail.com>
2024-06-11 01:09:22 +0500

    Add files via upload

---
+++ 14. Guest List/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let guest_list = ["Suma", "Mano", "Khsuhi"];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log("Dear Friend\n", guest_list[i], "\nIt's a great pleasure if you join me on tonight's dinner,", "Thank you");
+}
---
+++ 15. Changing Guest List/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest_list = ["Suma", "Mano", "Khsuhi"];
+// for (let i = 0; i < guest_list.length; i++) {
+//     console.log("Dear Friend\n" , guest_list[i], "\nIt's a great pleasure if you join me on tonight's dinner," , "Thank you");
+// }
+let absent_guest = "Khushi";
+let new_guest = "Naz";
+guest_list[2] = new_guest;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log("Dear Friend\n", guest_list[i], "\nIt's a great pleasure if you join me on tonight's dinner,", "Thank you");
+}
+console.log(absent_guest, "has another commitments, so she is not available for dinner.");
